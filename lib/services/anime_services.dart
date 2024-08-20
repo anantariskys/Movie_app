6fe665ecbce39242292bbe76f8ed79b90d326b6c
anantariskys <gamersindo555@gmail.com>
2024-08-20 18:33:40 +0700

    Update anime_services.dart

--- lib/services/anime_services.dart
+++ lib/services/anime_services.dart
@@ -5,13 +5,29 @@ import '../models/anime.dart';
 class AnimeServices {
   static const String baseUrl = 'https://api.jikan.moe/v4';
 
-  Future<List<Anime>> fetchAnimes() async {
-    final response = await http.get(Uri.parse('$baseUrl/anime'));
+  Future<List<Anime>> fetchAnimes({int page = 1, int limit = 6, String q =''}) async {
+    final uri = Uri.parse('$baseUrl/anime?page=$page&limit=$limit&q=$q');
+
+    final response = await http.get(uri);
 
     if (response.statusCode == 200) {
+      final Map<String, dynamic> jsonData = json.decode(response.body);
+      final List<dynamic> animesData = jsonData['data'];
+      return animesData.map((json) => Anime.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load animes');
+    }
+  }
 
-      final List<dynamic> jsonData = json.decode(response.body)['data'];
-      return jsonData.map((json) => Anime.fromJson(json)).toList();
+  Future<List<Anime>> fetchPopularAnimes({int page = 1, int limit = 7}) async {
+    final uri = Uri.parse('$baseUrl/top/anime?page=$page&limit=$limit');
+
+    final response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> jsonData = json.decode(response.body);
+      final List<dynamic> animesData = jsonData['data'];
+      return animesData.map((json) => Anime.fromJson(json)).toList();
     } else {
       throw Exception('Failed to load animes');
     }
