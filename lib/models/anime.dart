e6c019c0dd5e488a774861e237da38bb7cd5094c
anantariskys <gamersindo555@gmail.com>
2024-08-21 15:12:39 +0700

    Update anime.dart

--- lib/models/anime.dart
+++ lib/models/anime.dart
@@ -3,14 +3,18 @@ class Anime {
   final String title;
   final String synopsis;
   final String imageUrl;
-  Anime({required this.id, required this.title, required this.imageUrl,required this.synopsis});
+  final double score;
+  Anime({required this.id, required this.title, required this.imageUrl,required this.synopsis,required this.score});
 
   factory Anime.fromJson(Map<String, dynamic> json) {
     return Anime(
       synopsis: json['synopsis'],
       id: json['mal_id'],
       title: json['title'],
-      imageUrl: json['images']['jpg']['image_url'],
+      imageUrl: json['images']['jpg']['large_image_url'],
+      score: (json['score'] is int)
+          ? (json['score'] as int).toDouble()
+          : (json['score'] as num?)?.toDouble() ?? 0.0,
     );
   }
 }
