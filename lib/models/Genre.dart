bd79ef390dba1f64c7ac813fd0a6f0c767fcc14e
anantariskys <gamersindo555@gmail.com>
2024-08-23 21:56:27 +0700

    Create Genre.dart

---
+++ lib/models/Genre.dart
@@ -0,0 +1,22 @@
+class Genre {
+  final int malId;
+  final String type;
+  final String name;
+  final String url;
+
+  Genre({
+    required this.malId,
+    required this.type,
+    required this.name,
+    required this.url,
+  });
+
+  factory Genre.fromJson(Map<String, dynamic> json) {
+    return Genre(
+      malId: json['mal_id'],
+      type: json['type'],
+      name: json['name'],
+      url: json['url'],
+    );
+  }
+}
